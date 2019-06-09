cb8b7c2a3cbe25d7185e067b0b91aaf1b7bbd36e
Sceat <mr.sceat@outlook.com>
2019-06-09 14:35:08 +0300

    travis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import Telegraf from 'telegraf'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start(ctx => ctx.reply('Welcome'))
+const rand = () => Math.random() >= 0.5
+const mock = text =>
+	[...text].map(l => (rand() ? l.toLowerCase() : l.toUpperCase())).reduce((a, b) => `${a}${b}`, '')
 
+const dumbies = [
+	'https://imgur.com/kEylCE1',
+	'https://i.imgur.com/USnkQYL.png',
+	'https://imgur.com/3muAzdh',
+	'https://i.imgur.com/tk3dgbi.png',
+	'https://imgur.com/Gh7UmDl',
+	'https://imgur.com/q7ZWksb',
+	'https://imgur.com/tR1BtX7',
+	'https://i.imgur.com/x91kAkM.png',
+	'https://imgur.com/YRXudlD',
+	'https://imgur.com/iAXkgN0',
+	'https://i.imgur.com/ojPRquk.png',
+	'https://imgur.com/K29pMnu',
+	'https://imgur.com/KZ2t6dc',
+	'https://imgur.com/bCOUq64',
+	'https://imgur.com/qEsauQ8',
+	'https://imgur.com/sFAJxTy'
+]
+
+const randDumb = () => dumbies[Math.floor(Math.random() * dumbies.length)]
+
+bot.on('text', ctx => {
+	const msg = ctx.update?.message
+	const text = msg?.text
+	const reply = msg?.reply_to_message?.text
+	switch (text) {
+		case '/pls mock':
+			if (!reply) return
+			ctx.replyWithPhoto(randDumb())
+			ctx.reply(mock(reply))
+			break
+
+		default:
+			break
+	}
+})
 bot.launch()
