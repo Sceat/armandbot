7b4087b6552b5ee5ce66bb96fbde1d95e62fc9a7
Sceat <sceat@hydre.io>
2019-06-14 19:11:35 +0300

    allow outside learning

--- src/index.js
+++ src/index.js
@@ -257,9 +257,9 @@ const isReplyToArmand = msg =>
 
 bot.on('message', ctx => {
 	const msg = ctx.update?.message?.text
-	if (`${ctx.chat.id}` !== TG_ROOM) return
 	if (!msg) return
 	coll.then(coll => learnMsg(coll)(msg))
+	if (`${ctx.chat.id}` !== TG_ROOM) return
 	if (msg.startsWith('/c')) {
 		const last = replied.get('/c') || 0
 		if (randBool() && Date.now() > last + 1000 * 60 * 5) {
